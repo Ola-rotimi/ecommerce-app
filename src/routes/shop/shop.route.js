28e80f2d375232176383a05fb0e5693dc76b92f0
Olarotimi Adamson <olarotimiadamson@yahoo.com>
2023-09-10 16:52:41 +0100

    Added CartProvider and Cart components, updated navigation styling

--- src/routes/shop/shop.route.js
+++ src/routes/shop/shop.route.js
@@ -1,9 +1,20 @@
-const Shop = () =>{
-    return(
-        <div>
-            <h1>This is the shop component.</h1>
-        </div>
-    )
-}
+import { useContext } from "react";
+
+import { ProductContext } from "../../context/product.context";
+import ProductCard from "../../components/product-card/product-card.component";
+
+import "./shop.styles.scss";
+
+const Shop = () => {
+    const { products } = useContext(ProductContext);
+
+  return (
+    <div className="products-container">
+      {products.map((product) => (
+        <ProductCard key={product.id} product={product} />
+      ))}
+    </div>
+  );
+};
 
 export default Shop;
